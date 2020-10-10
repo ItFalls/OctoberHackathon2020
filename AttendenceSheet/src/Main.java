b43c6bd5e1c0a29c4159fda21b920885415e18e3
Ethan <ethanling123@gmail.com>
2020-10-10 16:25:25 -0700

    Added text field and launching chrome

--- AttendenceSheet/src/Main.java
+++ AttendenceSheet/src/Main.java
@@ -1,6 +1,7 @@
-
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static final int DRAWING_WIDTH = 1000; //Width of Window
@@ -15,6 +16,16 @@ public class Main {
         window.setExtendedState(window.getExtendedState() | JFrame.MAXIMIZED_BOTH);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        window.getContentPane().setLayout(new FlowLayout());
+        JTextField textfield1 = new JTextField("Text field 1",10);
+        JTextField textfield2 = new JTextField("Text field 2",10);
+        JTextField textfield3 = new JTextField("Text field 3",10);
+        window.getContentPane().add(textfield1);
+        window.getContentPane().add(textfield2);
+        window.getContentPane().add(textfield3);
+        window.pack();
+        window.setVisible(true);
+
         //Panel
         Runner panel = new Runner();
         panel.setBackground(background);
@@ -23,5 +34,20 @@ public class Main {
         window.setVisible(true);
         window.addKeyListener(panel);
         window.addMouseListener(panel);
+
+
+        Runtime runtime = Runtime.getRuntime();
+        System.out.print("What is your user folder's name?");
+        String username = new Scanner(System.in).nextLine();
+        String[] s = new String[] {"C:\\Users\\" + username + "\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe", "https://javaconceptoftheday.com/"};
+
+        try
+        {
+            runtime.exec(s);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
